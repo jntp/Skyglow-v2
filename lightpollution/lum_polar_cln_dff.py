3be651d5dce92c6b1c3c91d48204564ee5da1db0
Justin Tang <justin.jntprules@gmail.com>
2021-01-19 00:10:54 -0800

    added colormap configurations and axes and tick labels

--- lightpollution/lum_polar_cln_dff.py
+++ lightpollution/lum_polar_cln_dff.py
@@ -15,10 +15,14 @@ cont = xr.open_dataset("skg_swn_urb_cln.nc") # used to address "zero" issues in 
 diff_lmn = diff.lmn_bb_aa # broadband azimuthally averaged luminance
 cont_lmn = cont.lmn_bb_aa 
 levp = diff.levp # pressure coordinates 
-plr_dgr = cont.plr_dgr # polar angle in degrees
+plr_dgr = cont.plr_dgr # polar angle in radians
 
 # Divide difference data by control data and multiply by 100 to obtain percentage
 percentage = (diff_lmn / cont_lmn) * 100
+
+# Conver units for better readability
+levp = levp / 100 # convert to hectopascals
+plr_dgr = plr_dgr * 57.296 # convert to degrees
 
 # Swap values on plr_dgr to plot angles in the viewer's perspective instead of the light's
 plr_dgr = plr_dgr[::-1]
@@ -40,11 +44,21 @@ ax.clabel(lines, fmt = '%d', levels = levels)
   for txt in lines.labelTexts
 ]
 
+# Import Color map
+cmap = gvcmap.BlueRed 
+
+# Truncate colormap to only use the light blue colors and the reds
+cmap = gvutil.truncate_colormap(cmap, minval = 0.31, maxval = 1, n = 10) # on ncl 78-253
+
 # Plot filled contours
-colors = percentage.plot.contourf(ax = ax, levels = levels, cmap = gvcmap.BlueRed, add_labels = False, add_colorbar = False)
+colors = percentage.plot.contourf(ax = ax, levels = levels, cmap = cmap, add_labels = False, add_colorbar = False)
 
 # Add colorbar
 plt.colorbar(colors, ax = ax, orientation = 'vertical', ticks = levels[1::2], drawedges = True, aspect = 12, shrink = 0.7, pad = 0.1) # Look up slice notation on levels[1::2]
+
+# Use geocat.viz.util convenience function to set axes tick values
+# Set y-lim in order for y-axis to have descending values
+gvutil.set_axes_limits_and_ticks(ax, xlim = (0, 90), xticks = np.arange(0, 80, 20), ylim = (0, 1000), yticks = np.arange(0, 1000, 200))
 
 # Force the plot to be square by setting the aspect ratio to 1
 ax.set_box_aspect(1)
@@ -53,4 +67,4 @@ plt.tight_layout() # automatically adjust subplot parameters to give specified p
 plt.show() 
 
 
-
+# You left off on converting plr_dgr from radians to degrees???? 
